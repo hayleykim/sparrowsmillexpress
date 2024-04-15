5af16d518c90566314bee5830b6b0d8bbea28539
hayley <hayleyhkim@gmail.com>
2024-04-15 20:44:59 +1000

    homepage

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+CATEGORIES = (
+    ('R', 'Rice'),
+    ('N', 'Noodles'),
+    ('S', 'Soup'),
+    ('C', 'Chicken'),
+    ('x', 'Side')
+)
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.CharField(max_length=256)
+    category = models.CharField(
+        max_length=1,
+        choices=CATEGORIES,
+        default=CATEGORIES[0][0]
+    )
+    note = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Menu
 
 def home(request):
     return render(request, 'home.html')
@@ -6,3 +7,9 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def menu(request):
+    menus = Menu.objects.all()
+    return render(request, 'menu.html', {
+        'menus': menus
+    })
